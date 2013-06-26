9c2c54d41d504a155571c947213ce57354a37bc7
yuichi tanaka <u1tnk1123@gmail.com>
2013-06-26 21:22:09 +0900

    sugarcube用

--- app/main_view_controller.rb
+++ app/main_view_controller.rb
@@ -2,25 +2,20 @@ class MainViewController < UIViewController
   def viewDidLoad
     super
     marign = 20
-    view.backgroundColor = UIColor.whiteColor
+    view.backgroundColor = '#fff'.uicolor
     navigationItem.title = 'sample'
 
-    button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+    view << UIButton.rounded_rect.tap do |b|
       b.setTitle('Open Webview', forState:UIControlStateNormal)
       b.frame = [
         [marign, 100],
         [view.frame.size.width - marign * 2, 42],
       ]
-      b.addTarget(self, action: 'buttonDidPush', forControlEvents: UIControlEventTouchUpInside)
+      b.on(:touch) do
+        navigationController << WebViewController.new.tap do |wv|
+          wv.url = 'http://google.com'
+        end
+      end
     end
-
-    view.addSubview(button)
-  end
-
-  def buttonDidPush
-    web = WebViewController.new.tap do |wv|
-      wv.url = 'http://google.com'
-    end
-    navigationController.pushViewController(web, animated: true)
   end
 end
